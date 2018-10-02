a192eb629df17fde33313341a13cc621dab40a57
Gucci Gav <GAVIN@gavins-macbook-air.local>
2018-10-01 19:01:09 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Gavin de Soler on 10/1/18.
+//  Copyright © 2018 TheSocialInvestor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
